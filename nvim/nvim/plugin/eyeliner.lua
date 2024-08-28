071f4a397fd48420c4a65335598349057dbaf7ca
Yanis Fourel <yanis.fourel@ledr.io>
2024-08-28 10:41:51 +0800

    57 current  2024-08-28 10:41:47  24.05.20240822.797f7dc   6.6.46                          *

--- nvim/nvim/plugin/eyeliner.lua
+++ nvim/nvim/plugin/eyeliner.lua
@@ -3,5 +3,5 @@ require("eyeliner").setup {
 	dim = true,
 }
 
--- vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#5fffff", underline = true })
--- vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#999999", underline = true })
+vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#5fffff", underline = true })
+vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#999999", underline = true })
