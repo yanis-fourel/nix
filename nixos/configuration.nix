01cdc787c44cb3f0f91db66775ba27d2ae4a8b38
Yanis Fourel <git.xyybe@passmail.net>
2024-09-07 12:08:23 +0800

    109 current  2024-09-07 12:08:16  24.05.20240903.6f6c45b   6.6.48                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -55,11 +55,11 @@
     pkgs.man-pages
     pkgs.brightnessctl
     pkgs.ripgrep
-	pkgs.wl-clipboard
-	pkgs.fd
-	pkgs.tig
-	pkgs.rclone
-	pkgs.rsync
+    pkgs.wl-clipboard
+    pkgs.fd
+    pkgs.tig
+    pkgs.rclone
+    pkgs.rsync
     pkgs.gnupg
   ];
 
@@ -222,10 +222,6 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # List services that you want to enable:
 
