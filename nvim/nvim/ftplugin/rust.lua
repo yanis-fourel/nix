6e54ea25cf05e0ab7181366a791c0c0f4ad0e302
Yanis Fourel <git.xyybe@passmail.net>
2024-09-07 10:15:14 +0800

    84 current  2024-09-07 10:15:10  24.05.20240822.797f7dc   6.6.46                          *

--- nvim/nvim/ftplugin/rust.lua
+++ nvim/nvim/ftplugin/rust.lua
@@ -1,4 +1,11 @@
 
-require("lspconfig").rust_analyzer.setup({
+require("lspconfig").rust_analyzer.setup{
   capabilities = require('user.lsp').make_client_capabilities(),
-})
+  settings = {
+    ['rust-analyzer'] = {
+      diagnostics = {
+        enable = false;
+      }
+    }
+  }
+}
