204e456d33092109276e88c46b93c6486503a6c4
Yanis Fourel <git.xyybe@passmail.net>
2024-09-07 11:28:21 +0800

    98 current  2024-09-07 11:28:16  24.05.20240822.797f7dc   6.6.46                          *

--- nvim/nvim/plugin/conform.lua
+++ nvim/nvim/plugin/conform.lua
@@ -1,27 +1,29 @@
 -- autoformat
 
-require'conform'.setup {
-  notify_on_error = false,
-  format_on_save = function(bufnr)
-    -- Disable "format_on_save lsp_fallback" for languages that don't
-    -- have a well standardized coding style. You can add additional
-    -- languages here or re-enable it for the disabled ones.
-    local disable_filetypes = { c = true, cpp = true }
-    return {
-      timeout_ms = 500,
-      lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-    }
-  end,
-  formatters_by_ft = {
-    lua = { "stylua" },
-    c = { "clang-format" },
-    cpp = { "clang-format" },
+require("conform").setup({
+	notify_on_error = false,
+	format_on_save = function(bufnr)
+		-- Disable "format_on_save lsp_fallback" for languages that don't
+		-- have a well standardized coding style. You can add additional
+		-- languages here or re-enable it for the disabled ones.
+		local disable_filetypes = { c = true, cpp = true }
+		return {
+			timeout_ms = 500,
+			lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+		}
+	end,
+	formatters_by_ft = {
+		lua = { "stylua" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		rust = { "rustfmt" },
 
-    -- You can use a sub-list to tell conform to run *until* a formatter
-    -- is found.
-    -- javascript = { { "prettierd", "prettier" } },
-  },
-}
+		-- You can use a sub-list to tell conform to run *until* a formatter
+		-- is found.
+		-- javascript = { { "prettierd", "prettier" } },
+	},
+})
 
-vim.keymap.set("n", "<leader>w", function() require("conform").format({ async = true, lsp_fallback = true }) end, {desc = "Format buffer"})
-
+vim.keymap.set("n", "<leader>w", function()
+	require("conform").format({ async = true, lsp_fallback = true })
+end, { desc = "Format buffer" })
