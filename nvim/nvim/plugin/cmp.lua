fdd2c45240108fe117c109907f1d0ee2fa938305
Yanis Fourel <git.xyybe@passmail.net>
2024-09-07 12:33:54 +0800

    112 current  2024-09-07 12:33:48  24.05.20240903.6f6c45b   6.6.48                          *

--- nvim/nvim/plugin/cmp.lua
+++ nvim/nvim/plugin/cmp.lua
@@ -39,8 +39,8 @@ cmp.setup({
 		end, { "i", "s" }),
 	}),
 	sources = {
-		{ name = "nvim_lsp_signature_help" },
 		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
 		{ name = "luasnip" },
 		{ name = "path" },
 		{ name = "lazydev", group_index = 0 },
