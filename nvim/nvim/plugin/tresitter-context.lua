be273d0c1a24593163a0506360ecc4d79cf4dbe3
Yanis Fourel <git.xyybe@passmail.net>
2024-09-07 10:50:12 +0800

    92 current  2024-09-07 10:50:07  24.05.20240822.797f7dc   6.6.46                          *

--- nvim/nvim/plugin/tresitter-context.lua
+++ nvim/nvim/plugin/tresitter-context.lua
@@ -74,5 +74,5 @@ require'treesitter-context'.setup {
   mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
   -- Separator between context and content. Should be a single character string, like '-'.
   -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-  separator = nil,
+  separator = '-',
 }
