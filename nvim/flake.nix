e7235056945d1a76e802d62498b15387b3c698ea
Yanis Fourel <git.xyybe@passmail.net>
2024-09-23 10:01:50 +0800

    124 current  2024-09-21 18:43:22  24.05.20240903.6f6c45b   6.6.48                          *

--- nvim/flake.nix
+++ nvim/flake.nix
@@ -23,57 +23,44 @@
       ...
     }@inputs:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
+      system = "x86_64-linux";
       # This is where the Neovim derivation is built.
       neovim-overlay = import ./nix/neovim-overlay.nix { inherit inputs; };
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          # Import the overlay, so that the final Neovim derivation(s) can be accessed via pkgs.<nvim-pkg>
+          neovim-overlay
+          # This adds a function can be used to generate a .luarc.json
+          # containing the Neovim API all plugins in the workspace directory.
+          # The generated file can be symlinked in the devShell's shellHook.
+          gen-luarc.overlays.default
+        ];
+      };
+      shell = pkgs.mkShell {
+        name = "nvim-devShell";
+        buildInputs = with pkgs; [
+          # Tools for Lua and Nix development, useful for editing files in this repo
+          lua-language-server
+          nil
+          stylua
+          luajitPackages.luacheck
+        ];
+        shellHook = ''
+          # symlink the .luarc.json generated in the overlay
+          ln -fs ${pkgs.nvim-luarc-json} .luarc.json
+        '';
+      };
     in
-    flake-utils.lib.eachSystem supportedSystems (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            # Import the overlay, so that the final Neovim derivation(s) can be accessed via pkgs.<nvim-pkg>
-            neovim-overlay
-            # This adds a function can be used to generate a .luarc.json
-            # containing the Neovim API all plugins in the workspace directory.
-            # The generated file can be symlinked in the devShell's shellHook.
-            gen-luarc.overlays.default
-          ];
-        };
-        shell = pkgs.mkShell {
-          name = "nvim-devShell";
-          buildInputs = with pkgs; [
-            # Tools for Lua and Nix development, useful for editing files in this repo
-            lua-language-server
-            nil
-            stylua
-            luajitPackages.luacheck
-          ];
-          shellHook = ''
-            # symlink the .luarc.json generated in the overlay
-            ln -fs ${pkgs.nvim-luarc-json} .luarc.json
-          '';
-        };
-      in
-      {
-        packages = rec {
-          default = nvim;
-          nvim = pkgs.nvim-pkg;
-        };
-        devShells = {
-          default = shell;
-        };
-      }
-    )
-    // {
-      # You can add this overlay to your NixOS configuration
+    {
+      packages = rec {
+        default = nvim;
+        nvim = pkgs.nvim-pkg;
+      };
+      devShells = {
+        default = shell;
+      };
       overlays.default = neovim-overlay;
     };
 }
