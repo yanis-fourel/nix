c3f515062bf8ebaf7ec4c41883202861581d285b
Yanis Fourel <git.xyybe@passmail.net>
2024-09-28 14:18:34 +0800

    133 current  2024-09-28 14:18:27  24.05.20240903.6f6c45b   6.6.48                          *

--- hosts/yanix/config.nix
+++ hosts/yanix/config.nix
@@ -12,7 +12,23 @@
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   services.blueman.enable = true;
 
+  # This requires manually creating the /etc/davfs2/secrets file like:
+  # https://u425237.your-storagebox.de  <username>  <password>
   services.davfs2.enable = true;
+  systemd.mounts = [
+    {
+      enable = true;
+      description = "'sync' webdav mount point";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+
+      what = "https://u425237.your-storagebox.de";
+      where = "/mnt/mys3";
+      options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775";
+      type = "davfs";
+      mountConfig.TimeoutSec = 15;
+    }
+  ];
 
   nixpkgs.overlays = [
     inputs.fenix.overlays.default
