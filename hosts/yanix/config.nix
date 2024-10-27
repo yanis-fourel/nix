0f09e0d16dbdc5f9fb001a04e4f4917573cd48e7
Yanis Fourel <git.xyybe@passmail.net>
2024-10-27 09:19:18 +0100

    157 current  2024-10-27 09:19:09  24.11.20240926.1925c60   6.6.52                          *

--- hosts/yanix/config.nix
+++ hosts/yanix/config.nix
@@ -177,7 +177,7 @@
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "Asia/Taipei";
+  time.timeZone = "Europe/Paris";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
