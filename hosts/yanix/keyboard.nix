a8522a1c070b376eb5e0d6136825f844d185ceef
Yanis Fourel <git.xyybe@passmail.net>
2024-10-30 18:27:30 +0100

    162 current  2024-10-30 18:27:21  24.11.20240926.1925c60   6.6.52                          *

--- hosts/yanix/keyboard.nix
+++ hosts/yanix/keyboard.nix
@@ -3,7 +3,7 @@
   services.xserver.xkb.extraLayouts.qwerty-fr = {
     description = "US keyboard with french symbols - AltGr combination";
     languages = [
-      "eng"
+      "en"
       "fr"
     ];
     symbolsFile = /home/yanis/nixos/qwerty-fr;
