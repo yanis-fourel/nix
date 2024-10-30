1652b1b2ccc9d48aea95d1f2d85b22acfb87117f
Yanis Fourel <git.xyybe@passmail.net>
2024-10-30 12:16:29 +0100

    159 current  2024-10-30 12:16:20  24.11.20240926.1925c60   6.6.52                          *

--- hosts/yanix/keyboard.nix
+++ hosts/yanix/keyboard.nix
@@ -1,6 +1,11 @@
 { ... }:
 {
-  services.xserver = {
-    layout = "fr";
+  services.xserver.xkb.extraLayouts.qwerty-fr = {
+    description = "Variant of qwerty with extra symbols and diacritics so that typing both in French and English is easy and fast";
+    languages = [
+      "eng"
+      "fr"
+    ];
+    symbolsFile = /home/yanis/nixos/qwerty-fr;
   };
 }
