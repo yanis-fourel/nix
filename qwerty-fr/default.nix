8e1c551b3f1665b8f029fda39930e91be6ebbb3a
Yanis Fourel <git.xyybe@passmail.net>
2024-12-23 19:49:15 +0100

    add comment

--- qwerty-fr/default.nix
+++ qwerty-fr/default.nix
@@ -5,6 +5,16 @@
 
 stdenv.mkDerivation {
   name = "xkb-qwerty-fr";
+
+  # TODO: use
+  # src = fetchFromGitHub {
+  #   owner = "ColemakMods";
+  #   repo = "mod-dh";
+  #   rev = "e846a5bd24d59ed15ba70b3a9d5363a38ca51d09";
+  #   hash = "sha256-RFOpN+tIMfakb7AZN0ock9eq2mytvL0DWedvQV67+ks=";
+  #   sparseCheckout = [ "console" ];
+  # };
+
   src = ./qwerty-fr.xkb;
 
   # Override the unpack phase since this is a plain file, not an archive
