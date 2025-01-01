1ea1d9f06b6ffbd9fa9eefd7aad23dcba7af84bd
Yanis Fourel <yanis.fourel@ledr.io>
2025-01-01 15:32:04 +0100

    6 current   2025-01-01 12:40:45  25.05.20241219.d70bd19   6.6.66                          *

--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,7 @@ git add .
 sudo nix flake lock
 git add .
 echo "NixOS Rebuilding..."
-sudo nixos-rebuild --flake . switch # || (cat nixos-switch.log | grep --color error && false)
+sudo nixos-rebuild --flake .#ledr-yanix switch # || (cat nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations | grep current)
 git add .
 git commit -m "$gen"
